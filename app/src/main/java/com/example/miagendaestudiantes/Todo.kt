b94f94cbde1bf8fd2ebf56812f0f935201bbc737
cepeda03 <miguelcepeda257@gmail.com>
2025-11-24 19:23:46 -0300

    COMMIT

--- app/src/main/java/com/example/miagendaestudiantes/Todo.kt
+++ app/src/main/java/com/example/miagendaestudiantes/Todo.kt
@@ -3,8 +3,9 @@ package com.example.miagendaestudiantes
 data class Todo(
     var id: String = "",
     var text: String = "",
+    var completed: Boolean = false,
+    var userId: String = "",
     var enabled: Boolean = true,
     var createdAt: Long = 0L,
-    var completed: Boolean = false,
     var finalDate: String = ""
 )
